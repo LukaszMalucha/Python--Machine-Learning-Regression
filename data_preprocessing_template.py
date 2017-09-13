c0aab6c290391b4659f8000b5abd496a0d85f7aa
Lukasz Malucha <lucasmalucha@gmail.com>
2017-09-13 15:20:58 +0100

    Update data_preprocessing_template.py

--- data_preprocessing_template.py
+++ data_preprocessing_template.py
@@ -21,3 +21,25 @@ X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
+
+
+                              ###### OPTIONAL ######
+
+
+# Taking care of missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+
+# Encoding categorical data
+# Encoding the Independent Variable
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()                             ## Categorical data encoded into number
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0])             
+onehotencoder = OneHotEncoder(categorical_features = [0])   ## Encode countries into 1-0-0 to not affect equation.
+X = onehotencoder.fit_transform(X).toarray()
+# Encoding the Dependent Variable
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
